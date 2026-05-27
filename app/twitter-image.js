8c54275c6ae515dc9c070f27fe4b38f4bd36d4e6
consensusgurus <consensusgurus@gmail.com>
2026-05-27 16:32:47 -0400

    deleted duplicates

--- app/twitter-image.js
+++
@@ -1,6 +0,0 @@
-import Image from './opengraph-image';
-export const runtime = 'edge';
-export const alt = 'Consensus Gurus — Top Ten Lists from Every Angle';
-export const size = { width: 1200, height: 630 };
-export const contentType = 'image/png';
-export default Image;
